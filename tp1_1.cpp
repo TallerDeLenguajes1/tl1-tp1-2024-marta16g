09699bf650d40a2a41a0714e5ebe2680d3d27c00
marta16g <martugutierrez166@gmail.com>
2024-03-26 13:32:26 -0300

    mostrando por pantalla lo pedido en punto 3e

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int main (){
-    
+
+    int pepa = 50;
+    int *puntero;
+    puntero = &pepa;
+
+
+    printf("El contenido del puntero: %d \n", *puntero);
+    printf("La dirección de memoria que guarda el puntero: %d \n", puntero);
+    printf("Dirección de memoria del puntero: %d \n", &puntero);
+    // printf("Dirección de memoria de la variable: %d \n" &pepa);
+    printf("Tamanio de la memoria utilizado por la variable: %d \n", sizeof(pepa));
     printf("Hola Mundo");
 
     return 0;
